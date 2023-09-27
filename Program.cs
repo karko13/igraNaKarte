020d16c3434717c6492bb508b453f97685834006
karko13 <145532713+karko13@users.noreply.github.com>
2023-09-27 08:44:20 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace igraNaKarte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int karte = 0;
+            int CardCounter = 0;
+            while (CardCounter < 31)
+            {
+                Console.WriteLine(" unesi kartu ");
+                karte = Convert.ToInt32(Console.ReadLine());
+                if(karte <= 13 && karte >= 1 )
+                {
+                    CardCounter += karte;
+                    
+                }
+                else
+                {
+                    Console.WriteLine(" unjeli ste prevelik ili premali broj ");
+                }
+            }
+            if(CardCounter == 31 )
+                    {
+                        Console.WriteLine(" POBJEDA!!!");
+                    }
+                    else
+                    {
+                        Console.WriteLine(" IZGUBILI STE :( ");
+                    }
+            Console.ReadKey();
+        }
+    }
+}
